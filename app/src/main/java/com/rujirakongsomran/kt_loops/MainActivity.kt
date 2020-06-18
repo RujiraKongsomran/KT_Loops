1a7f7e41e32771c8e0a46591189b7e291000e9b6
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-18 14:46:14 +0700

    For Loop

--- app/src/main/java/com/rujirakongsomran/kt_loops/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_loops/MainActivity.kt
@@ -2,10 +2,31 @@ package com.rujirakongsomran.kt_loops
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlayString.setOnClickListener {
+            // For loop
+            val items1 = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            for (index in items1)
+                Log.d("DEBUG", "" + index)
+
+            val items2 = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            for (index in items2.indices)
+                Log.d("DEBUG", "index : $index = ${items2[index]}")
+
+            val items3 = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            for ((index, value) in items3.withIndex())
+                Log.d("DEBUG", "index : $index = $value")
+
+
+
+        }
+
     }
 }
